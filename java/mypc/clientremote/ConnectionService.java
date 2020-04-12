5457523017f641eeabbe5c93ec614533ab3dd624
Pablo Fernandez <pablo.fernandez@cscs.ch>
2020-04-12 12:47:20 +0200

    Added license file

--- java/mypc/clientremote/ConnectionService.java
+++ java/mypc/clientremote/ConnectionService.java
@@ -25,12 +25,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
-import java.net.InetSocketAddress;
 import java.net.NoRouteToHostException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
